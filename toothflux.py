b72a476b9a7d4048ed2a920129ce716cf2ae0c4b
garobed1 <garo.bedonian@gmail.com>
2019-07-11 16:08:50 -0400

    debug and test

--- toothflux.py
+++ toothflux.py
@@ -16,6 +16,9 @@ def toothflux(theta, params, phi):
     t_mag = params['t_mag'] #magnet thickness
     r_m = params['rot_or'] #rotor outer radius (INCLUDING MAGNETS)
     gap = params['gap'] #air gaps
+    t_mag = params['t_mag'] #magnet thickness
+    tt = params['tt']
+    ts = params['ts']
     alpha = 1/30 #magnetic fraction, fraction of rotor circumference occupied by one magnet
     
     r_r = r_m - t_mag
@@ -27,8 +30,7 @@ def toothflux(theta, params, phi):
     
     n_p = n_m/2 #number of pole pairs
     
-    ts = 2*pi/n_s #slot to slot angle
-    tt = #tooth width angle
+    
 
     Kslm = np.zeros(mterms + 1)
     Bgn = np.zeros(nterms + 1)
@@ -46,10 +48,12 @@ def toothflux(theta, params, phi):
         #first determine g(theta)/g
         #gratio = gfunc(theta, gap, r_s, n_m, tt, ts)
         
-        Ksl = kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, l_m)
+        #Ksl = kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, l_m)
         
         #fourier transform to obtain coeffificients in Kslm
         #compute using Riemann sum
+        
+        Kslm = FourierCoeffsNumpy(kfunc, T, mterms,  gap, r_s, n_m, tt, ts, mu_r, t_mag)
         
         #slot correction term layer
         for m in range(-mterms, mterms):
@@ -76,6 +80,8 @@ def toothflux(theta, params, phi):
         
         phi += Bgn[n+nterms]*(2*pi*l_st*r_s/n_s)*Ksl*exp(1j*n*theta)
         
+    
+    #Bt = phi/(k_st*l_st*w_tb)
     
 def kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, t_mag):
     
@@ -112,24 +118,51 @@ def RadialR(n, alpha):
 def RadialT(n, alpha):
     return 0
 
-def FourierCoeffsNumpy(func, T, N):
+def FourierCoeffsNumpy(func, T, N,  gap, r_s, n_m, tt, ts, mu_r, t_mag):
     #func: one-dimensional function of interest
     #T: period of function, will compute from center
-    #N: number of terms 1 and onward, this computes 2*N + 1 coefficients
+    #N: number of terms 1 and onward, this computes N + 1 coefficients
     
     fsample = 2*N 
     t = np.linspace(-T/2, T/2, fsample + 2)
-    y = np.fft.rfft(func(t))
+    y = np.fft.rfft(func(t, gap, r_s, n_m, tt, ts, mu_r, t_mag))/t.size
     
+    c = y
+    for k in range(1, N):
+        np.insert(c, 0, np.conj(y(k)))
+    
+    return c
 
-if name == '__main__'
+if __name__ == '__main__':
 
-    gap = .003
+    gap = .001
     r_s = .07
     n_m = 20
-    tt = pi/10
-    ts = 1.1*pi/10
-    mu_r = .001
+    n_s = 24
+    ts = 2*pi/n_s #slot to slot angle 
+    tt = .6*pi/10 #tooth width angle
+    mu_r = 1
+    t_mag = .003
+    l_st = .008
     
-    period = ts
+    params = {
+        'gap':gap,
+        'n_m':n_m,
+        'n_s':n_s,
+        'tt':tt,
+        'ts':ts,
+        'mu_r':mu_r,
+        't_mag':t_mag,
+        'sta_ir':r_s,
+        'l_st':l_st
+    }
     
+    theta = ts/2 # np.linspace(-ts,ts)
+    
+    T = ts
+    N = 5
+    
+    x = kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, t_mag)
+    y = FourierCoeffsNumpy(kfunc, T, N, gap, r_s, n_m, tt, ts, mu_r, t_mag)
+    
+    print(x)
