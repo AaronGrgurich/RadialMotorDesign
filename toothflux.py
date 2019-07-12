b6bca22bfe23cb8af1fddb6540838c374418ba45
garobed1 <garo.bedonian@gmail.com>
2019-07-12 14:51:08 -0400

    can get answer, need to debug

--- toothflux.py
+++ toothflux.py
@@ -8,10 +8,12 @@ import numpy as np
 from math import pi, sin, cos, exp
 import matplotlib.pyplot as plt
 
-def toothflux(theta, params, phi):
+def toothflux(theta, nterms, params, Bn):
     n_m = params['n_m'] #number of poles
     n_s = params['n_s'] #number of Slots
     l_st = params['l_st'] #axial motor length
+    k_st = params['k_st'] #lamination stacking factor
+    w_tb = params['w_tb'] #tooth body width
     r_s = params['sta_ir'] #stator inner radius
     t_mag = params['t_mag'] #magnet thickness
     r_m = params['rot_or'] #rotor outer radius (INCLUDING MAGNETS)
@@ -25,21 +27,23 @@ def toothflux(theta, params, phi):
     
     b_r = 1.3 #magnetic remanence
     
-    nterms = 5 #flux fourier series terms
-    mterms = 5 #slot correction fourier series terms
+    #nterms = 10 #flux fourier series terms
+    mterms = 100 #slot correction fourier series terms
     
     n_p = n_m/2 #number of pole pairs
     
     
 
-    Kslm = np.zeros(mterms + 1)
-    Bgn = np.zeros(nterms + 1)
+    Kslm = np.zeros(2*mterms + 1)
+    Bgn = np.zeros(2*nterms + 1)
+    phi = np.zeros(2*nterms + 1)
+    Bn = np.zeros(2*nterms + 1)
     
     mu_r = .001 #magnet relative recoil permeability, aka relative permitivity, mu/mu_0
     
     phi = 0
     #flux term layer
-    for n in range(-nterms, nterms):
+    for n in range(-nterms, nterms+1):
         beta = n*n_p
         
         slotsum = 0
@@ -56,12 +60,14 @@ def toothflux(theta, params, phi):
         Kslm = FourierCoeffsNumpy(kfunc, T, mterms,  gap, r_s, n_m, tt, ts, mu_r, t_mag)
         
         #slot correction term layer
-        for m in range(-mterms, mterms):
-            
-            #Kslm[m + mterms] = 
-            
-            slotsum += Kslm[m + mterms]*sin(pi*(m + n*(n_m/(2*n_s))))/(pi*(m + n*(n_m/(2*n_s))))
-            
+        if n%2 != 0:
+            for m in range(-mterms, mterms + 1):
+
+                slotsum += Kslm[m + mterms]*sin(pi*(m + n*(n_m/(2*n_s))))/(pi*(m + n*(n_m/(2*n_s))))
+        
+        else:
+            slotsum = 0
+
         #
         # add k_rn and k_tn here based on magnetic field orientation of halbach array
         #
@@ -72,16 +78,19 @@ def toothflux(theta, params, phi):
         k_mc = (k_rn + 1j*beta*k_tn)/(1 - beta**2)
         
         del_r = (mu_r + 1)*((r_r/r_m)**(2*beta) - (r_s/r_m)**(2*beta)) + (mu_r - 1)*(1 -((r_r/r_m)**(2*beta))*((r_s/r_m)**(2*beta)))
+        #print(del_r)
+        if n != 0:
+            k_a = (k_mc*k_rn/del_r)*((1-(r_r/r_m)**(2*beta))*((beta + 1)*(r_r/r_m)**(2*beta) - (2*beta)*(r_r/r_m)**(beta+1) + beta - 1))
+        else:
+            k_a = 0
         
-        k_a = (k_mc*k_rn/del_r)*((1-(r_r/r_m)**(2*beta))*((beta + 1)*(r_r/r_m)**(2*beta) - (2*beta)*(r_r/r_m)**(beta+1) + beta - 1))
-        
+        k_a = k_a.real
+        print(n)
         Bgn[n+nterms] = -b_r*k_a*(((r_s/r_m)**(beta-1)) + ((r_s/r_m)**(2*beta))*((r_m/r_s)**(beta+1)))
-            
-        
-        phi += Bgn[n+nterms]*(2*pi*l_st*r_s/n_s)*Ksl*exp(1j*n*theta)
-        
-    
-    #Bt = phi/(k_st*l_st*w_tb)
+                    
+        Bn[n+nterms] = Bgn[n+nterms]*(2*pi*l_st*r_s/n_s)*slotsum/(k_st*l_st*w_tb)
+        print(Bn[n+nterms])
+    return Bn
     
 def kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, t_mag):
     
@@ -126,13 +135,14 @@ def FourierCoeffsNumpy(func, T, N,  gap, r_s, n_m, tt, ts, mu_r, t_mag):
     #N: number of terms 1 and onward, this computes N + 1 coefficients
     
     fsample = 2*N 
-    t = np.linspace(-T/2, T/2, fsample + 2)
-    y = np.fft.rfft(func(t, gap, r_s, n_m, tt, ts, mu_r, t_mag))/t.size
+    t = np.linspace(-T/2, T/2, fsample + 1, endpoint = False)
+    y = np.fft.rfft(func(t, gap, r_s, n_m, tt, ts, mu_r, t_mag))/fsample
     
     c = y
-    for k in range(1, N):
-        np.insert(c, 0, np.conj(y[k]))
+    for k in range(1, N + 1):
+        c = np.insert(c, 0, np.conj(y[k]))
     
+    c = c.real
     return c
 
 if __name__ == '__main__':
@@ -146,6 +156,9 @@ if __name__ == '__main__':
     mu_r = 1
     t_mag = .003
     l_st = .008
+    k_st = 1
+    w_tb = .003
+    rot_or = .05
     
     params = {
         'gap':gap,
@@ -156,16 +169,33 @@ if __name__ == '__main__':
         'mu_r':mu_r,
         't_mag':t_mag,
         'sta_ir':r_s,
-        'l_st':l_st
+        'l_st':l_st,
+        'k_st':k_st,
+        'w_tb':w_tb,
+        'rot_or':(r_s-gap)
     }
     
-    theta = np.linspace(-ts/2,ts/2)
+    theta = 0#np.linspace(-ts/2,ts/2)
     
     T = ts
-    N = 5
+    N = 40
     
-    x = kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, t_mag)
-    y = FourierCoeffsNumpy(kfunc, T, N, gap, r_s, n_m, tt, ts, mu_r, t_mag)
+    # x = kfunc(theta, gap, r_s, n_m, tt, ts, mu_r, t_mag)
+    # y = FourierCoeffsNumpy(kfunc, T, N, gap, r_s, n_m, tt, ts, mu_r, t_mag)
     
-    print(x)
-    print(y)
+    Bn = np.zeros(2*N + 1)
+
+    
+    Bn = toothflux(theta, N, params, Bn)
+    
+    #print(Bn)
+    
+    Brn = np.concatenate((Bn[40:], Bn[0:39]))
+    #print(Brn)
+    
+    i = np.fft.ifft(Brn)
+    
+    #print(i)
+    
+    # print(x)
+    # print(y)
